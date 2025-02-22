d84562dc5478beaae4506d0bdec41e973ab55240
Oleskasi <oleska0pelmeska@gmail.com>
2025-02-22 15:58:54 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,3 +1,6 @@
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
 
 	const showSlider = new Swiper('.showcase-carousel', {
@@ -33,40 +36,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const menuButton = document.getElementById('menuButton');
-    const menu = document.getElementById('menu');
-    const body = document.body;
-    let isOpen = false;
-
-    if (!menuButton || !menu || !body) {
-        console.error('One or more elements not found');
-        return;
-    }
-
-    const toggleMenu = (event) => {
-        event.preventDefault();
-        body.classList.toggle('open');
-        isOpen = !isOpen;
-    };
-
-    menuButton.addEventListener('click', toggleMenu);
-    menuButton.addEventListener('touchstart', toggleMenu);
-
-    // Close menu when clicking outside
-    window.addEventListener('click', function(event) {
-        if (isOpen && event.target !== menuButton && !menu.contains(event.target)) {
-            body.classList.remove('open');
-            isOpen = false;
-        }
-    });
-
-    window.addEventListener('touchstart', function(event) {
-        if (isOpen && event.target !== menuButton && !menu.contains(event.target)) {
-            body.classList.remove('open');
-            isOpen = false;
-        }
-    });
+document.getElementById('menuButton').addEventListener('click', function() {
+	document.body.classList.toggle('open');
+});
+document.querySelector('.backdrop').addEventListener('click', function() {
+	document.body.classList.remove('open');
 });
